aad63d77a56dc6df568278ebba17556e768c851d
mariotinoco517 <tinocomario517@gmail.com>
2025-04-18 12:17:49 -0500

    Start of project client

---
+++ Project3Client/src/main/java/GuiClient.java
@@ -0,0 +1,94 @@
+
+import java.util.HashMap;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class GuiClient extends Application{
+
+	
+	TextField c1;
+	Button b1;
+	HashMap<String, Scene> sceneMap;
+	VBox clientBox;
+	Client clientConnection;
+
+	HBox fields;
+
+	ComboBox<Integer> listUsers;
+	ListView<String> listItems;
+
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		clientConnection = new Client(data->{
+				Platform.runLater(()->{
+					switch (data.type){
+						case NEWUSER:
+							listUsers.getItems().add(data.recipient);
+							listItems.getItems().add(data.recipient + " has joined!");
+							break;
+						case DISCONNECT:
+							listUsers.getItems().remove(data.recipient);
+							listItems.getItems().add(data.recipient + " has disconnected!");
+							break;
+						case TEXT:
+							listItems.getItems().add(data.recipient+": "+data.message);
+					}
+			});
+		});
+							
+		clientConnection.start();
+
+		listUsers = new ComboBox<Integer>();
+		listUsers.getItems().add(-1);
+		listUsers.setValue(-1);
+		listItems = new ListView<String>();
+
+		
+		c1 = new TextField();
+		b1 = new Button("Send");
+		fields = new HBox(listUsers,b1);
+		b1.setOnAction(e->{clientConnection.send(new Message(listUsers.getValue(), c1.getText())); c1.clear();});
+
+		clientBox = new VBox(10, c1,fields,listItems);
+		clientBox.setStyle("-fx-background-color: blue;"+"-fx-font-family: 'serif';");
+
+
+
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
+
+		primaryStage.setScene(new Scene(clientBox, 400, 300));
+		primaryStage.setTitle("Client");
+		primaryStage.show();
+		
+	}
+	
+
+
+}
